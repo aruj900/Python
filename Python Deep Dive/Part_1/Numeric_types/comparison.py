ef7e3c38426b976d31b16456f5a8dfb2c59c8f6b
aruj900 <arujmahajan900@gmail.com>
2020-07-21 15:45:02 -0400

    booleanC:/Users/aruj/Anaconda3/python.exe

---
+++ Python Deep Dive/Part_1/Numeric_types/comparison.py
@@ -0,0 +1,13 @@
+print( 3 in [1,2,3])
+
+print([1,2] is [1,2]) # Due to different memory address
+
+# Comparison doesn't work with complex numbers
+
+print(3 < 2 < 1/0)
+
+#This is due to shortcircuiting as 3 < 2 and 2 < 1/0 so second is never evaluated
+
+'A' < 'a' # Capital is less than lowercase
+
+ 
