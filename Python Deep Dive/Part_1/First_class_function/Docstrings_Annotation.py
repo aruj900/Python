4b0c220d8292af7ae7ebc3dd7d93225578e9c9a4
aruj900 <arujmahajan900@gmail.com>
2020-07-23 20:36:52 -0400

    Docstrings

---
+++ Python Deep Dive/Part_1/First_class_function/Docstrings_Annotation.py
@@ -0,0 +1,21 @@
+def my_func(a,b=1):
+    "returns a*b"
+    return a*b
+help(my_func)
+
+def my_func1(a:'annotation for a',b: 'annotation for b'= 1) -> 'something with a long annotation':
+    """documentation"""
+    print(a*b)
+    
+help(my_func1)
+
+x = 3
+y = 5
+def my_func3(a:'some character') -> 'character a repeated ' + str(max(x,y)) + ' times' :
+    return a* max(x,y)
+
+help(my_func3)
+
+x = 10
+print(my_func3('a'))
+help(my_func3)
