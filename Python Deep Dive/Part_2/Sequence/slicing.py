dfb45d44797440444e527759dcac7b85c0964956
aruj900 <arujmahajan900@gmail.com>
2020-08-14 01:12:40 -0700

    slicing

---
+++ Python Deep Dive/Part_2/Sequence/slicing.py
@@ -0,0 +1,22 @@
+s = slice(0,2)
+print(type(s))
+
+print(s.start)
+
+print(s.stop)
+
+l = [1,2,3,4,5,6]
+
+print(l[s])
+
+print(l[0:100])
+
+print(l[0:6:2])
+
+print(slice(1,5))
+
+s = slice(1,5)
+
+print(s.indices(10))
+
+print(slice(0,100,2).indices(10))
