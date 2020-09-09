8664d16eee8a6ccedf932b1638ab1996c92a05e5
aruj900 <arujmahajan900@gmail.com>
2020-09-08 21:52:53 -0700

    medium

---
+++ Algoexpert/Medium/RiverSizes/dfs_elegant.py
@@ -0,0 +1,36 @@
+def dfs(i,j):
+    currentLength = 1
+    stack = [(i,j)]
+    grid[i][j] = '0'
+    while stack:
+        x,y = stack.pop()
+        for direction in [(1,0),(0,1),(-1,0),(0,-1)]:
+            new_x, new_y = x + direction[0], y + direction[1]
+            if new_x >= 0 and new_x < len(grid) and new_y >= 0 and new_y < len(grid[0]) and grid[new_x][new_y]=='1':
+                currentLength += 1
+                grid[new_x][new_y]= '0'
+                stack.append((new_x,new_y))
+    return currentLength
+            
+    
+
+def numIsland(grid):
+    lengthArray = []
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '1':
+                a = dfs(i,j)
+                lengthArray.append(a)
+                count +=1
+    return lengthArray
+
+grid = [
+  ["1","1","0","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","1","1"]
+]
+
+
+print(numIsland(grid))
