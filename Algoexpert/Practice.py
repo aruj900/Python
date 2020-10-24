9495e43f42f56ef6eb5a80d1a8305079836e1a48
TCS Admin <tcsadmin@Aruj-MacBook-Pro.local>
2020-10-24 00:53:43 -0700

    hard

--- Algoexpert/Practice.py
+++ Algoexpert/Practice.py
@@ -1,20 +1,2 @@
-def maxPathSum(tree):
-    _,maxSum = finMaxSum(tree)
-    return maxSum
-
-def findMaxSum(tree):
-    if tree is None:
-        return (float("-inf"),float("-inf"))
+def longestCommonSubsequence(str1,str2):
     
-    leftMaxSumAsBranch, leftMaxPathSum = findMaxSum(tree.left)
-    rightMaxSumAsBranch,rightMaxPathSum = findMaxSum(tree.right)
-
-    maxChildSumAsBranch = max(leftMaxSumAsBranch,rightMaxSumAsBranch)
-
-    value = tree.value
-    maxSumAsBranch = max(maxChildSumAsBranch + value, value)
-    maxSumAsRootNode = max(leftMaxSumAsBranch + value + rightMaxSumAsBranch, maxSumAsBranch)
-    maxPathSum = max(leftMaxPathSum,rightMaxPathSum,maxSumAsRootNode)
-
-    return (maxSumAsBranch, maxPathSum)
-
